22e0f79c513b2943763dfec82a05a97be37aeb31
Tej Patel <tejpatel317@gmail.com>
2022-10-25 19:11:26 -0400

    commit tree a9d41e6b59010cf67a60545188acbe1b9eae06c8

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :must_have_clickbait_title
+
+    PATTERNS = [ /Won't Believe/i , /Secret/i , /Top \d/i , /Guess/i]
+
+
+    def must_have_clickbait_title
+        condition = PATTERNS.none? do |t|
+            t.match title
+        end
+        if condition
+            errors.add(:title, "must be clickbait")
+        end
+    end
 end
